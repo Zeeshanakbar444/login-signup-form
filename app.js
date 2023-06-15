514877d991e0158251b2ac81df66ef261fdf33c2
Akbarzeeshan <akbarzeeshan444@gmail.com>
2023-06-15 22:21:08 +0500

    login form complete

---
+++ app.js
@@ -0,0 +1,73 @@
+function signUp(e) {
+    e.preventDefault();
+
+    var fullName = document.querySelector('#fullName').value;
+    var password = document.querySelector('#password').value;
+    var email = document.querySelector('#email').value;
+    var number = document.querySelector('#number').value;
+
+    var obj = {
+        fullName,
+        password,
+        email,
+        number
+    };
+
+    var getUsers = JSON.parse(localStorage.getItem('users'));
+
+    if (getUsers === null) {
+        var arr = [];
+        arr.push(obj);
+        console.log('first user signup')
+        localStorage.setItem('users', JSON.stringify(arr));
+        alert('uer signup')
+        window.location.href = "./index.html"
+    } else {
+        console.log('grtuseer ', getUsers)
+        var findUser = getUsers.find(function (value) {
+            console.log(value.email);
+            if (value.email === email) {
+
+                return true
+            }
+        })
+        if (findUser === undefined) {
+            getUsers.push(obj);
+            localStorage.setItem('users', JSON.stringify(getUsers));
+            alert('signup')
+            window.location.href = "./index.html"
+        } else {
+            alert('email address already exisr')
+        }
+
+    }
+}
+
+function login(e) {
+    e.preventDefault();
+    var loginEmail = document.querySelector('#loginEmail').value;
+    var loginPassword = document.querySelector('#loginPassword').value;
+
+    var getUsers = JSON.parse(localStorage.getItem('users'));
+   
+    var userIndex = getUsers.findIndex(function (value) {
+        if (value.email === loginEmail && value.password === loginPassword) return true
+
+    })
+    if (userIndex !== -1) {
+        console.log('succsess fully login');
+        alert('succsess fully login')
+        window.location.replace('./facebook.html')
+    } else {
+        console.log('your email is not exist ');
+        alert('your email is not exist')
+    }
+    
+
+}
+
+
+
+
+
+
