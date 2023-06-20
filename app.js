12e5a7c8ace81058d227e46ba214f7beb7300812
Akbarzeeshan <akbarzeeshan444@gmail.com>
2023-06-20 09:24:08 +0500

    post complete

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+
+var loginUser;
+var listParent = document.getElementById("listParent")
+
+window.addEventListener("load", function () {
+    console.log("hello world ")
+    var getUsers = JSON.parse(localStorage.getItem("users"))
+    console.log(getUsers, "getUser")
+    loginUser = getUsers
+
+// console.log(loginUser.fullName)
+    var fullName = document.getElementById("fullName")
+    console.log("fullName", fullName)
+    if (fullName) {
+        // fullName.innerText = "WELCOME" + " " + loginUser.fullName
+
+    }
+    if (listParent) {
+        var getPosts = JSON.parse(localStorage.getItem("posts"))
+        for (var value of getPosts) {
+            listParent.innerHTML += `<div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${value.title}</h5>
+                <p class="card-text">${value.desc}</p>
+                <button class="btn btn-info">EDIT</button>
+                <button class="btn btn-danger">DELETE</button>
+            </div>
+        </div>`
+        }
+
+    }
+
+})
+
+
 function signUp(e) {
     e.preventDefault();
 
@@ -50,16 +85,16 @@ function login(e) {
 
     var getUsers = JSON.parse(localStorage.getItem('users'));
     console.log(getUsers)
-    if (getUsers === null) {
-        alert('your email is not exist')
-        location.href = './signup.html'
-    }
+    // if (getUsers === null) {
+    //     alert('your email is not exist')
+    //     location.href = './signup.html'
+    // }
     var userIndex = getUsers.find(function (i) {
 
         if (i.email === loginEmail && i.password === loginPassword) return true
 
     })
-    if (userIndex !== undefined) {
+    if (userIndex !== null) {
         console.log('succsess fully login');
         alert('succsess fully login')
         window.location.replace('./facebook.html')
@@ -71,6 +106,55 @@ function login(e) {
 }
 
 
+
+
+// post
+function addPost() {
+    console.log('add todo')
+    var title = document.getElementById("title")
+    var desc = document.getElementById("desc")
+if(!title.value || !desc.value){
+    alert("Please enter values")
+    return
+}
+
+var todoBox = `<div class="card" style="width: 18rem;">
+<div class="card-body">
+    <h5 class="card-title">${title.value}</h5>
+    <p class="card-text">${desc.value}</p>
+
+    <button class="btn btn-info">EDIT</button>
+    <button class="btn btn-danger">DELETE</button>
+
+</div>
+</div>`
+// console.log(todoBox, "todoBox")
+listParent.innerHTML += todoBox
+
+var postObj = {
+    title: title.value,
+    desc: desc.value
+}
+
+
+var getPosts = JSON.parse(localStorage.getItem("posts"))
+console.log("getPosts", getPosts)
+
+if (getPosts == null) {
+    var array = []
+    array.push(postObj)
+    localStorage.setItem("posts", JSON.stringify(array))
+
+} else {
+    getPosts.unshift(postObj)
+    localStorage.setItem("posts", JSON.stringify(getPosts))
+
+}
+
+title.value = ""
+desc.value = ""
+
+}
 
 
 
